76926ecb3bbc02d706d73db22b818f45038a0df5
aleksnick <aleksnick@list.ru>
2012-10-04 23:22:46 +0400

    I am need a global params in js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,28 @@
 
 $(function(){
+	var conf={};
+});
+
+$(function(){
+	$.ajax({
+		url:"conf/main.json",
+		dataType:"json",
+		success:function(s){
+			window.conf.main=s;
+		}
+	});
+});
+
+$(function(){
+	
+});
+
+
+$(function(){
 	$(".bar-button").on("click",function(){
-		alert("go!");
+		//alert("go!");
+		
+			alert(window.conf.main.packet.region);
 	});
 
 });
