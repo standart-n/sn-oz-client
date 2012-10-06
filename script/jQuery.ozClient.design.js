34221679282e786a628cfd21d413b15f32d860bb
aleksnick <aleksnick@list.ru>
2012-10-06 18:30:52 +0400

    начал думать над шаблонами

---
+++ script/jQuery.ozClient.design.js
@@ -0,0 +1,56 @@
+(function($){
+
+	var methods={
+		init:function(options)
+		{
+			return this.each(function(){
+				var sn=$(this).data('ozClient');
+				if (sn.design){
+					$(this).ozClientDesign('side');
+				}
+			});
+		},
+		side:function()
+		{
+			var s="";
+			var sn=$(this).data('ozClient');
+			if (sn.design.side) {
+				$.each(sn.design.side,function(i){
+					if (this.type=="box") {
+						s+='<div class="side-box-outer">';
+						s+='<div class="side-box">';
+						$.each(this.links,function(i){
+							if (this.type=="simple-link") {
+								this.cl="side-box-link";
+								if (!this.active) { this.active=false; }
+								if (!this.underline) { this.underline=false; }
+								if (this.active) { this.cl+=" side-box-link-active"; } else { this.cl+=" side-box-link-normal"; }
+								s+='<a href="'+this.href+'" class="'+this.cl+'">'+this.caption+'</a>';
+								if (this.underline) {
+									s+='<div class="side-box-line"></div>';
+								}
+							}
+						});						
+						s+='</div>';
+						s+='</div>';
+					}
+				});
+			}
+			if (s!="") {
+				$("#side-content").html(s);
+			}
+		}
+	};		
+
+	$.fn.ozClientDesign=function(sn){
+		if (!sn) { var sn={}; }
+		if ( methods[sn]) {
+			return methods[sn].apply(this,Array.prototype.slice.call(arguments,1));
+		} else if (typeof sn==='object' || !method) {
+			return methods.init.apply(this,arguments);
+		} else {
+			$.error('Метод '+sn+' в jQuery.ozClient не существует');
+		}    
+		
+	};		
+})(jQuery);
