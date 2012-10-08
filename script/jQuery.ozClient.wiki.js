9b28579180349979ba643847f3f17768c50b95c1
aleksnick <aleksnick@list.ru>
2012-10-08 13:27:44 +0400

    начинаю делать вики разметку в текстах

---
+++ script/jQuery.ozClient.wiki.js
@@ -0,0 +1,26 @@
+(function($){
+
+	var methods={
+		init:function(options)
+		{
+			return this.each(function(){
+			});
+		},
+		parse:function()
+		{
+			//
+		}
+	};		
+
+	$.fn.ozClientWiki=function(sn){
+		if (!sn) { var sn={}; }
+		if ( methods[sn]) {
+			return methods[sn].apply(this,Array.prototype.slice.call(arguments,1));
+		} else if (typeof sn==='object' || !sn) {
+			return methods.init.apply(this,arguments);
+		} else {
+			$.error('Метод '+sn+' не существует');
+		}    
+		
+	};		
+})(jQuery);
