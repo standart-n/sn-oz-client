92b0888ef19af07e3fe39bea52dda3a5586eeec8
aleksnick <aleksnick@list.ru>
2012-11-01 19:05:40 +0400

    добавил раздел вакансии, исправил несколько ошибок

--- script/jQuery.ozClient.events.js
+++ script/jQuery.ozClient.events.js
@@ -24,7 +24,7 @@
 						sn.part="main";
 						setTimeout('$(this).ozClientDesign("pie_all");',1);
 					break;
-					case "main":
+					default:
 						var l_two=href.replace(/(.*)#(.*?)\/(.*?)\/(.*)/,"$3");
 						$("html,body").animate({scrollTop:0},0);
 						if (l_one!=sn.part) {
@@ -32,7 +32,7 @@
 							$(this).ozClientModels('side',{'file':l_one+'.html'});
 							$(this).ozClientTriggers('linksSide');
 							$(this).ozClientTriggers('switcherSide');
-							sn.part="main";
+							sn.part=l_one;
 							setTimeout('$(this).ozClientDesign("pie_side");',1);
 						}						
 						switch (l_two) {
@@ -43,34 +43,13 @@
 								setTimeout('$(this).ozClientDesign("pie_primary");',1);
 							break;
 						}
-					break;
-					case "news":
-						var l_two=href.replace(/(.*)#(.*?)\/(.*?)\/(.*)/,"$3");
-						$("html,body").animate({scrollTop:0},0);
-						if (l_one!=sn.part) {
-							$(this).ozClientTriggers('switchBar',{'link':l_one});
-							$(this).ozClientModels('side',{'file':l_one+'.html'});
-							$(this).ozClientTriggers('linksSide');
-							$(this).ozClientTriggers('switcherSide');
-							sn.part="news";
-							setTimeout('$(this).ozClientDesign("pie_side");',1);
-						}						
-						switch (l_two) {
-							case "text":
-								var l_three=href.replace(/(.*)#(.*?)\/(.*?)\/(.*?)\/(.*)/,"$4");
-								$(this).ozClientTriggers('switchSide',{'link':l_three});
-								$(this).ozClientModels('primary',{'file':l_three+'.html'});
-								setTimeout('$(this).ozClientDesign("pie_primary");',1);
-							break;
-						}
-					break;
 				}
 				$(this).data('ozClient',sn);
 				var anchor=href.replace(/(.*)\:(.*?)\/(.*)/,"$2");
 				if (anchor!="") {
 					var height=$('#anchor-'+anchor).offset().top-87;
-					if (height>0) {
-						
+					if (height) {
+						//alert(anchor);
 						if ($("#side-"+anchor).length) {
 							$(this).ozClientTriggers('switchSide',{'link':anchor});
 						}
